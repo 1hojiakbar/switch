50f7dddde7ab4cfc7632b058be8bbfaf9686dc19
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-04-01 22:47:18 +0500

    if else

---
+++ if-else/index.js
@@ -0,0 +1,18 @@
+// if
+
+if (condition) {
+  console.log("Shart true bo'lsa ekranga chiqaman");
+}
+
+// ====================
+
+// if else
+if (condition) {
+  console.log("if");
+} else if (condition) {
+  console.log("else if");
+} else if (condition) {
+  console.log("else if 2");
+} else {
+  console.log("else");
+}
