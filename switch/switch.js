85dcc6b7785f3d4edf0b577747b429be684dd054
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-04-01 22:44:42 +0500

    switch

---
+++ switch/switch.js
@@ -0,0 +1,14 @@
+// switch
+
+let num = 10;
+
+switch (num) {
+  case 10:
+    console.log("Bu son 10 ga teng");
+    break;
+  case 15:
+    console.log("Bu son 15 ga teng");
+    break;
+  default:
+    console.log("Hech qaysi case ga to'gri kelmasa men ekranga chiqaman");
+}
