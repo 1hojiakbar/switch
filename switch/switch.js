02feb6e08efb53683b6455217b97ed0f43d955de
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-04-01 22:45:30 +0500

    switch

--- switch/switch.js
+++ switch/switch.js
@@ -12,3 +12,23 @@ switch (num) {
   default:
     console.log("Hech qaysi case ga to'gri kelmasa men ekranga chiqaman");
 }
+
+// ===================================================
+
+let browser = "Firefox";
+
+switch (browser) {
+  case "Edge":
+    console.log("You've got the Edge!");
+    break;
+
+  case "Chrome":
+  case "Firefox":
+  case "Safari":
+  case "Opera":
+    console.log("Okay we support these browsers too");
+    break;
+
+  default:
+    console.log("We hope that this page looks ok!");
+}
